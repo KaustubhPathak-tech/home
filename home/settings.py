5bf86c50885abce32346347638b11f2817e67029
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2025-12-24 22:59:31 +0530

    debugged wsgi application

--- home/settings.py
+++ home/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1xh-bk3_y-hq$h=v!5o%gh)nqzgs5bnh!^(sx69+*6ecoh6kzd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
