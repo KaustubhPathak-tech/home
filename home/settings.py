6da0f679b4f28964aeca00a425628a92afdfa67b
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2025-12-25 00:19:58 +0530

    updated db

--- home/settings.py
+++ home/settings.py
@@ -89,13 +89,12 @@ WSGI_APPLICATION = 'home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if os.getenv("VERCEL"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": "/tmp/db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
 
 
 # Password validation
